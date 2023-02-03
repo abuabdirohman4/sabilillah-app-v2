b663ea0d6b83b93f291faf7bd80a2c543a402fa3
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-02-03 08:03:27 +0700

    update css link in resources

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,8 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Absensi Brangsong</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
+    <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
+    <script src="{{ asset('build/assets/app.js') }}"></script>
+    {{-- @vite('resources/css/app.css')
+    @vite('resources/js/app.js') --}}
     @livewireStyles
 </head>
 
