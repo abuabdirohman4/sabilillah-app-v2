59d4b75a0a0b72c659c5f8880bd9ec41fb167697
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-02-03 08:09:12 +0700

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,15 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Absensi Brangsong</title>
-    <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
-    <script src="{{ asset('build/assets/app.js') }}"></script>
-    {{-- @vite('resources/css/app.css')
-    @vite('resources/js/app.js') --}}
+    {{-- <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
+    <script src="{{ asset('build/assets/app.js') }}"></script> --}}
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
     @livewireStyles
 </head>
 
-<body class="p-4 bg-gray-100 h-full max-w-lg mx-auto">
-    <div>
+<body class=" bg-gray-100">
+    <div class="p-4 h-full max-w-lg mx-auto">
         <x-navbar />
         <main>
             @yield('content')
