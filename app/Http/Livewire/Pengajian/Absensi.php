61210fe9e5933442fd2571d99c4e3f1ad0c20bc4
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-02-05 14:38:28 +0700

    add prettier

--- app/Http/Livewire/Pengajian/Absensi.php
+++ app/Http/Livewire/Pengajian/Absensi.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 
 class Absensi extends Component
 {
+    public $data = 0;
     public function render()
     {
         return view('livewire.pengajian.absensi');
--- database/seeders/SantriSeeder.php
+++ database/seeders/SantriSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Santri;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class SantriSeeder extends Seeder
 {
@@ -14,25 +15,22 @@ class SantriSeeder extends Seeder
      */
     public function run()
     {
-       for ($i = 0; $i < 3; $i++) {
-                Surat::create([
-                    "surat_dari" => fake()->name(),
-                    "jenis_surat" => "Surat Masuk",
-                    "no_surat" => fake()->phoneNumber(),
-                    "tanggal_surat" => fake()->dateTime(),
-                    "sifat" => $sifat[array_rand($sifat)],
-                    "no_agenda" => fake()->buildingNumber(),
-                    "tanggal_kegiatan" => fake()->dateTime(),
-                    "kategori" => fake()->streetName(),
-                    "perihal" => fake()->realText(20),
-                    "file" => fake()->mimeType(),
-                    "file_name" => "Surat Masuk.docx",
-                    "disposisi" => $disposisi[array_rand($disposisi)],
-                    "status" => "",
-                    "diteruskan_ke" => $unit,
-                    "catatan" => fake()->realText(10),
-                    "dari" => fake()->name(),
-                ]);
+        for ($i = 0; $i < 3; $i++) {
+            Santri::create([
+                "surat_dari" => fake()->name(),
+                "jenis_surat" => "Surat Masuk",
+                "no_surat" => fake()->phoneNumber(),
+                "tanggal_surat" => fake()->dateTime(),
+                "no_agenda" => fake()->buildingNumber(),
+                "tanggal_kegiatan" => fake()->dateTime(),
+                "kategori" => fake()->streetName(),
+                "perihal" => fake()->realText(20),
+                "file" => fake()->mimeType(),
+                "file_name" => "Surat Masuk.docx",
+                "status" => "",
+                "catatan" => fake()->realText(10),
+                "dari" => fake()->name(),
+            ]);
         }
     }
 }
